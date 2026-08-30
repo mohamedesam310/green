75fe0fce9150818dab446bac03f4cfa854cea077
Mohamed Esam <mmmesam742@gmail.com>
2026-08-31 01:20:42 +0300

    Move project to repository root

---
+++ green/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/green/',
+  server: {
+    port: 5173,
+    open: true,
+  },
+})
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
